592a8865aa22c12d1f73f84b64c40154279eae79
Lucas Yamamoto <17356081+lucasayb@users.noreply.github.com>
2024-01-08 14:25:37 -0300

    checkpoint

--- handler.py
+++ handler.py
@@ -1,11 +1,24 @@
 import json
+import io
+import cgi
 
-
-def hello(event, context):
+def converter(event, context):
+    print(event)
+    # file = io.BytesIO(event['body'].encode('utf-8'))
+    # pdict = cgi.parse_header(event['headers']['content-type'])[1]
+    # print('pdict', pdict)
+    # if 'boundary' in pdict:
+    #     pdict['boundary'] = pdict['boundary'].encode('utf-8')
+    # pdict['CONTENT-LENGTH'] = event['headers']['content-length']
+    # form_data = cgi.parse_multipart(file, pdict)
+    # print(str(form_data['file']))
     body = {
         "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
     }
+    # body = {
+    #     "message": "Go Serverless v3.0! Your function executed successfully!",
+    #     "input": str(form_data['file']),
+    # }
 
     response = {"statusCode": 200, "body": json.dumps(body)}
 
